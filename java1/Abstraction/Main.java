1584beaddb5ed2f7116824f5c730ecf4d151b00a
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-10-01 13:46:47 +0530

    Create Main.java

---
+++ java1/Abstraction/Main.java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		Car car = new Car();
+//		Vehicle vehicle = new Vehicle(); //we cannot create the object of Vehicle class since its an abstract class.
+		
+		car.go();// calling the overridden method go()
+		
+	}
+
+}
