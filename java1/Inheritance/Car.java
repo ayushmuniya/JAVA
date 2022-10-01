c58d8e596f92e27961b28eead27f94990f27d9d0
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-10-01 13:39:49 +0530

    Create Car.java

---
+++ java1/Inheritance/Car.java
@@ -0,0 +1,7 @@
+
+public class Car extends Vehicle // inheriting the attributes of Vehicle class
+{
+	int wheels = 4; // additional attributes that Car class has
+	int doors = 4; // additional attributes that Car class has
+
+}
