d0dce6238dca4d617d01d8654cca36cfc6c9b992
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-10-01 13:47:14 +0530

    Create Vehicle.java

---
+++ java1/Abstraction/Vehicle.java
@@ -0,0 +1,7 @@
+
+public abstract class Vehicle //abstract class
+{
+	abstract void go();// abstract method does not have a body
+	//this forces us to implement this method into each of the child class of Vehicle class 
+	// so we will override the go() method in child class 'Car'
+}
