85528b85c074c3f6fc1e0e69d94250c344932b79
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-10-01 13:55:45 +0530

    Create Hawk.java

---
+++ java1/Interface/Hawk.java
@@ -0,0 +1,13 @@
+
+public class Hawk implements Predator // Hawk is going to use this interface called Predator
+{
+
+	@Override
+	public void hunt()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("the hawk is hunting");
+		
+	}
+
+}
