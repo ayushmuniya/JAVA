280e9822bcacf98e532b2c9738954da8351dc002
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-10-01 13:39:27 +0530

    Create Vehicle.java

---
+++ java1/Inheritance/Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle
+{
+	double speed;
+
+	void go()
+	{
+		System.out.println("this vehicle is moving");
+	}
+
+	void stop()
+	{
+		System.out.println("this vehicle is stop");
+	}
+
+}
