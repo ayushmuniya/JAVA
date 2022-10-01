ffd5126ee5c7c6f07bdc513653d0299b0963717d
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-10-01 13:50:44 +0530

    Create C.java

---
+++ java1/AccessModifier/package2/C.java
@@ -0,0 +1,9 @@
+package package2;
+import package1.*;
+
+public class C
+{
+	public String publicMessage = "this is public";
+	       String defaultMessage = "this is default"; // no access modifier used in this variable
+	protected String protectedMessage = "this is protected";
+}
