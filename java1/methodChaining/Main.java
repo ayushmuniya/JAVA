22130a76329e8579794d3a257e97df1705ea3744
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-10-02 21:51:46 +0530

    Create Main.java

---
+++ java1/methodChaining/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		
+		Dog dog = new Dog();
+		dog.speak(); //when we call the speak method, the overridden method will be called as now 'Dog' class has its own speak() method
+		//output = the dog goes bark
+		
+		Animal animal = new Animal();
+		animal.speak(); //this will call the method in the 'Animal' class
+		//output = the animal speaks
+
+	}
+
+}
