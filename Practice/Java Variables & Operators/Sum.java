a98fb95ee45fe13392b1efb4664dd666e570e4fd
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-11-05 13:54:25 +0530

    Sum

---
+++ Practice/Java Variables & Operators/Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int sumOfTwoNumbers(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String args[]) {
+        assert (sumOfTwoNumbers(100, 566) == 666) : "Expect 666 for a = 100, b = 566";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
